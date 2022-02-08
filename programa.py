073494f301169b6a81bbfabcc56a214ef78bd12d
Gerson dos Reis de Melo <gerson.jp.otaku@gmail.com>
2022-02-07 22:08:38 -0300

    New project
    Mostra se os dois números digitados estão em ordem crescente ou decrescente, o programa só para quando digitado o mesmo número

---
+++ programa.py
@@ -0,0 +1,14 @@
+
+
+
+
+x = int(input("Digite dois numeros: \n"))
+y = int(input());
+
+while x != y:
+    if x < y:
+        print("CRESCENTE")
+    else:
+        print("DECRESCENTE")
+    x = int(input("Digite outros dois numeros: \n"))
+    y = int(input());
